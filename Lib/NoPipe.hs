0cd3fd222a69ec512d5c2d7000df48bcf5a971b1
Andrew Frank <frank@geoinfo.tuwien.ac.at>
2017-05-03 00:48:59 +0200

    get status and decide on dir or file

--- Lib/NoPipe.hs
+++ Lib/NoPipe.hs
@@ -25,24 +25,24 @@ import   System.FilePath.Posix  ((</>))
 processOneDirEntry :: FilePath -> IO [String]
 processOneDirEntry fp = do
     stat <- P.getFileStatus fp
+    res1 <- if P.isRegularFile stat
+        then do
+            processFile fp
+        else do
+            processDir fp
+--    let res2 =   unwords ["\n?: ", fp]
 
-    let res2 =   unwords ["\n?: ", fp]
-
-    return [res2]
+    return res1
 
 
-
-
-
-
-processFile :: FilePath -> IO String
+processFile :: FilePath -> IO [String]
 -- ^ process one file - print filename as a stub
 processFile fn = do
     putStrLn . unwords $ ["processFile - ", fn]
     s <- readFile fn
     let md = 0
     let res = unwords ["\nF:", fn, show md]
-    return res
+    return [res]
 
 processDir :: FilePath -> IO [String]
 -- ^ process one directory
@@ -53,7 +53,8 @@ processDir dir = do
     -- exclude special files:
     let content2 = filter ( \file' -> (file' /= "." && file' /= "..")  ) content
     -- complete the names to absolute filepath
-    let content3 = map (dir </>) content
+    let content3 = map (dir </>) content2
+    -- process each entry
     res3 <- mapM processOneDirEntry content3
     return (res1 : concat res3)
 
@@ -61,7 +62,25 @@ processDir dir = do
 
 test_1 = do
     res <- processDir "/home/frank/testFileIO"
-    assertEqual res_3a res
+    assertEqual res_4a res
+
+res_4a =
+    ["\nD:  /home/frank/testFileIO",
+     "\nF: /home/frank/testFileIO/a1.txt 0",
+     "\nD:  /home/frank/testFileIO/sub.d",
+     "\nF: /home/frank/testFileIO/sub.d/a1.txt 0",
+     "\nD:  /home/frank/testFileIO/sub.d/.hiddensub.d",
+     "\nF: /home/frank/testFileIO/sub.d/.hiddensub.d/a1.txt 0",
+     "\nF: /home/frank/testFileIO/sub.d/.hiddensub.d/.a4.hidden 0",
+     "\nF: /home/frank/testFileIO/sub.d/.hiddensub.d/a2 0",
+     "\nF: /home/frank/testFileIO/sub.d/.hiddensub.d/a3 0",
+     "\nF: /home/frank/testFileIO/sub.d/.a4.hidden 0",
+     "\nF: /home/frank/testFileIO/sub.d/a2 0",
+     "\nF: /home/frank/testFileIO/sub.d/a3 0",
+     "\nF: /home/frank/testFileIO/.a4.hidden 0",
+     "\nF: /home/frank/testFileIO/a2 0",
+     "\nD:  /home/frank/testFileIO/subnew",
+     "\nF: /home/frank/testFileIO/a3 0"]
 
 res_3a =
     ["\nD:  /home/frank/testFileIO",
