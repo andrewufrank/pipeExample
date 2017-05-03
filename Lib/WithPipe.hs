76889254f3278b6a2aafde5f25fec64bb9e49a4d
Andrew Frank <frank@geoinfo.tuwien.ac.at>
2017-05-03 20:34:15 +0200

    moved unnecessary code to end -- keep the code with >-> and move for .. to end

--- Lib/WithPipe.hs
+++ Lib/WithPipe.hs
@@ -42,88 +42,24 @@ startPipe :: FilePath ->  IO String
 -- ^ collect the filenames and md5
 startPipe fp = do
     res <- Pipe.runEffect $  effect8
-----                Pipe.yield fp
-----                fp
---        ~> processOneDirEntry
-----        ~> Pipe.print
---                ~> Pipe.toListM
-
     return "test" -- res
 
-effect1 :: Effect IO ()
-effect1 = for stdinLn $ \str ->
-                lift $ putStrLn str
-
-effect2 :: Effect IO ()
-effect2 = for (initialProducer "/home/frank/Workspace8/testDirFileIO") $ \dir ->
-                lift $ putStrLn dir
-
-effect3 :: Effect IO ()
-effect3 = for stdinLn $ \str ->
-                finalConsumer str
-
-effect4 :: Effect IO ()
-effect4 = for stdinLn $ \str ->
-                putStrConsumer str
-
-effect5 :: Effect IO ()
-effect5 =  for stdinLn
-                finalConsumer
-
-------effect6 :: Effect IO ()
---effect6 =   P.stdinLn  >-> Pipe.toListM  - possibly problem with toListM
-
-effect7 :: Effect IO ()
-effect7 =  (initialProducer "/home/frank/Workspace8/testDirFileIO")
-                >-> P.stdoutLn
 
 effect8 :: Effect IO ()
 effect8 =  (initialProducer "/home/frank/Workspace8/testDirFileIO")
                 >-> processOneDirEntry0
-                >-> P.stdoutLn
+                >-> finalConsumer0
 
-
-tut1 :: Effect IO ()
-tut1 =  P.stdinLn >-> P.stdoutLn
---stdinLn :: Producer String IO ()
-stdinLn = do
-    eof <- lift S.isEOF        -- 'lift' an 'IO' action from the base monad
-    unless eof $ do
-        str <- lift getLine
-        yield str            -- 'yield' the 'String'
-        stdinLn              -- Loop
-
---initialProducer :: FilePath -> Pipe.Producer String IO ()
-initialProducer fp = do
-    Pipe.yield fp
-    return ()
 
 --finalConsumer :: String -> Consumer String IO ()
-finalConsumer st = do
+finalConsumer0 = do
+    st <- await
     Pipe.lift $ putStrLn st
 --    return (a:: _)
 
 --putStrConsumer :: String -> Consumer String IO ()
 putStrConsumer st =  lift $ putStrLn st
 
---processOneDirEntry :: FilePath -> IO [String]
-processOneDirEntry :: FilePath -> Pipe FilePath String IO ()
---processOneDirEntry ::  Pipe FilePath String IO ()
-processOneDirEntry fp = do
---    fp <- Pipe.await
-    stat <- Pipe.lift $ Posix.getFileStatus fp
-    let isRegular = Posix.isRegularFile stat
-    if isRegular
-        then do
-            res <- Pipe.lift $ processFile1 fp
-            Pipe.yield res
-        else do
---            content ::[FilePath]  <- Pipe.lift $ processDir1 fp
---            Pipe.each content -- processOneDirEntry
-        --             processDir fp
-            Pipe.yield ("dir " ++ fp)
---    processOneDirEntry
-    return ()
 
 --processOneDirEntry :: FilePath -> IO [String]
 --processOneDirEntry :: FilePath -> Pipe FilePath String IO ()
@@ -182,9 +118,79 @@ processDir1 dir = do
 
 
 test_1 = do
+    putStrLn "----------------------------"
 --    res <- processDir "/home/frank/Workspace8/testDirFileIO"
     startPipe "/home/frank/Workspace8/testDirFileIO"
-    assertBool False
+    putStrLn "============================"
+    assertBool False  -- to force output
+
+
+------- old examples
+effect1 :: Effect IO ()
+effect1 = for stdinLn $ \str ->
+                lift $ putStrLn str
+
+effect2 :: Effect IO ()
+effect2 = for (initialProducer "/home/frank/Workspace8/testDirFileIO") $ \dir ->
+                lift $ putStrLn dir
+
+effect3 :: Effect IO ()
+effect3 = for stdinLn $ \str ->
+                finalConsumer str
+
+effect4 :: Effect IO ()
+effect4 = for stdinLn $ \str ->
+                putStrConsumer str
+
+effect5 :: Effect IO ()
+effect5 =  for stdinLn
+                finalConsumer
+
+------effect6 :: Effect IO ()
+--effect6 =   P.stdinLn  >-> Pipe.toListM  - possibly problem with toListM
+
+effect7 :: Effect IO ()
+effect7 =  (initialProducer "/home/frank/Workspace8/testDirFileIO")
+                >-> P.stdoutLn
+
+
+tut1 :: Effect IO ()
+tut1 =  P.stdinLn >-> P.stdoutLn
+--stdinLn :: Producer String IO ()
+stdinLn = do
+    eof <- lift S.isEOF        -- 'lift' an 'IO' action from the base monad
+    unless eof $ do
+        str <- lift getLine
+        yield str            -- 'yield' the 'String'
+        stdinLn              -- Loop
+
+--initialProducer :: FilePath -> Pipe.Producer String IO ()
+initialProducer fp = do
+    Pipe.yield fp
+    return ()
 
 
 
+--finalConsumer :: String -> Consumer String IO ()
+finalConsumer st = do
+    Pipe.lift $ putStrLn st
+--    return (a:: _)
+
+--processOneDirEntry :: FilePath -> IO [String]
+processOneDirEntry :: FilePath -> Pipe FilePath String IO ()
+--processOneDirEntry ::  Pipe FilePath String IO ()
+processOneDirEntry fp = do
+--    fp <- Pipe.await
+    stat <- Pipe.lift $ Posix.getFileStatus fp
+    let isRegular = Posix.isRegularFile stat
+    if isRegular
+        then do
+            res <- Pipe.lift $ processFile1 fp
+            Pipe.yield res
+        else do
+--            content ::[FilePath]  <- Pipe.lift $ processDir1 fp
+--            Pipe.each content -- processOneDirEntry
+        --             processDir fp
+            Pipe.yield ("dir " ++ fp)
+--    processOneDirEntry
+    return ()
