839538be95e3c48b73ec9384243123758ff9d45a
Andrew Frank <frank@geoinfo.tuwien.ac.at>
2017-05-03 21:36:57 +0200

    cleaned still running

--- Lib/WithPipe.hs
+++ Lib/WithPipe.hs
@@ -41,17 +41,11 @@ import Lib.NoPipe (getMD5, res_6a)
 startPipe :: FilePath ->  IO String
 -- ^ collect the filenames and md5
 startPipe fp = do
-    res <- Pipe.runEffect $  effect9
+    res <- Pipe.runEffect $  effect9 fp
     return "test" -- res
 
-
-effect8 :: Effect IO ()
-effect8 =  (initialProducer "/home/frank/Workspace8/testDirFileIO")
-                >-> processOneDirEntry0
-                >-> finalConsumer0
-
-effect9 :: Effect IO ()
-effect9 =  for (initialProducer "/home/frank/Workspace8/testDirFileIO") $ \dir ->
+effect9 :: FilePath -> Effect IO ()  -- useful to fix types
+effect9 fp =  for (initialProducer fp) $ \dir ->
                 processOneDirEntry dir
                 >-> finalConsumer0
 
@@ -66,39 +60,11 @@ finalConsumer0 = do
     st <- await
     Pipe.lift $ putStrLn st
     finalConsumer0
---    return (a:: _)
-
---putStrConsumer :: String -> Consumer String IO ()
-putStrConsumer st =  lift $ putStrLn st
 
 
---processOneDirEntry :: FilePath -> IO [String]
 --processOneDirEntry :: FilePath -> Pipe FilePath String IO ()
-processOneDirEntry0 ::  Pipe FilePath String IO ()
-processOneDirEntry0   = do
-    fp <- Pipe.await
-    Pipe.yield ("start " ++ fp)
-    stat <- Pipe.lift $ Posix.getFileStatus fp
-    let isRegular = Posix.isRegularFile stat
-    if isRegular
-        then do
-            res <- Pipe.lift $ processFile1 fp
-            Pipe.yield res
-        else do
-            Pipe.yield ("startDir " ++ fp)
-            content ::[FilePath]  <- Pipe.lift $ processDir1 fp
-            lift $ putStrLn . show $ content
-            Pipe.each content -- processOneDirEntry
-        --             processDir fp
---            Pipe.yield ("dir " ++ fp)
-    processOneDirEntry0
-    return ()
-
---processOneDirEntry :: FilePath -> IO [String]
---processOneDirEntry :: FilePath -> Pipe FilePath String IO ()
---processOneDirEntry ::  Pipe FilePath String IO ()
+-- must not have a defined type
 processOneDirEntry fp = do
---    fp <- Pipe.await
     stat <- Pipe.lift $ Posix.getFileStatus fp
     let isRegular = Posix.isRegularFile stat
     if isRegular
@@ -106,40 +72,25 @@ processOneDirEntry fp = do
             res <- Pipe.lift $ processFile1 fp
             Pipe.yield res
         else do
-            Pipe.yield ("startDir " ++ fp)
+            let res1 = unwords ["\nD: ", fp]
+            Pipe.yield res1
             content ::[FilePath]  <- Pipe.lift $ processDir1 fp
-            lift $ putStrLn . show $ content
-            mapM_ processOneDirEntry content
---    processOneDirEntry
+--            lift $ putStrLn . show $ content
+            mapM_ processOneDirEntry content  --   recursion
     return ()
-
-
-----processDir :: FilePath -> IO [String]
----- ^ process one directory
---processDir ::  FilePath ->    Pipe.Pipe String FilePath IO ()
---processDir dir = do
-----    putStrLn . unwords $ ["processDirectory - ", dir]
-----    dir <- Pipe.await
---    content ::[FilePath]  <- Pipe.lift $ processDir1 dir
---
---    Pipe.each content -- processOneDirEntry
---
---    return () -- yRes
 
 ----------------------------------------------support code (not pipes)
 processFile1 :: FilePath -> IO String
 -- ^ process one file - print filename as a stub
 processFile1 fn = do
---    putStrLn . unwords $ ["processFile - ", fn]
---    s <- readFile fn
     md <-  getMD5 fn
-    let res = unwords ["\nF:", fn, show md]
+    let res = unwords ["F:", fn, show md]
     return res
 
 --processDir1 :: FilePath -> IO [FilePath ]
 -- ^ process one directory
 processDir1 dir = do
-    let res1 = unwords ["\nD: ", dir]
+    let res1 = unwords ["D: ", dir]
     content <- D.getDirectoryContents dir
     -- exclude special files:
     let content2 = filter ( \file' -> (file' /= "." && file' /= "..")  ) content
@@ -151,56 +102,8 @@ processDir1 dir = do
 
 test_1 = do
     putStrLn "----------------------------"
---    res <- processDir "/home/frank/Workspace8/testDirFileIO"
     startPipe "/home/frank/Workspace8/testDirFileIO"
     putStrLn "============================"
     assertBool False  -- to force output
 
-
-------- old examples
-effect1 :: Effect IO ()
-effect1 = for stdinLn $ \str ->
-                lift $ putStrLn str
-
-effect2 :: Effect IO ()
-effect2 = for (initialProducer "/home/frank/Workspace8/testDirFileIO") $ \dir ->
-                lift $ putStrLn dir
-
-effect3 :: Effect IO ()
-effect3 = for stdinLn $ \str ->
-                finalConsumer str
-
-effect4 :: Effect IO ()
-effect4 = for stdinLn $ \str ->
-                putStrConsumer str
-
-effect5 :: Effect IO ()
-effect5 =  for stdinLn
-                finalConsumer
-
-------effect6 :: Effect IO ()
---effect6 =   P.stdinLn  >-> Pipe.toListM  - possibly problem with toListM
-
-effect7 :: Effect IO ()
-effect7 =  (initialProducer "/home/frank/Workspace8/testDirFileIO")
-                >-> P.stdoutLn
-
-
-tut1 :: Effect IO ()
-tut1 =  P.stdinLn >-> P.stdoutLn
---stdinLn :: Producer String IO ()
-stdinLn = do
-    eof <- lift S.isEOF        -- 'lift' an 'IO' action from the base monad
-    unless eof $ do
-        str <- lift getLine
-        yield str            -- 'yield' the 'String'
-        stdinLn              -- Loop
-
-
-
-
---finalConsumer :: String -> Consumer String IO ()
-finalConsumer st = do
-    Pipe.lift $ putStrLn st
---    return (a:: _)
 
