886230754bc5edb482846d5e1ea75f97aa392aea
Andrew Frank <frank@geoinfo.tuwien.ac.at>
2017-05-11 23:22:24 +0200

    cleaned

--- Lib/PipeUsingUniform.hs
+++ Lib/PipeUsingUniform.hs
@@ -2,7 +2,7 @@
 --
 -- Module      :  collect all files on disk
 -- and compute for each the MD5
--- with pipe
+-- with pipe  - as an test for path
 
 
 -----------------------------------------------------------------------------
@@ -15,7 +15,7 @@
     , TypeFamilies
     , ScopedTypeVariables
     , OverloadedStrings
-    , PackageImports
+--    , PackageImports
     #-}
 
 
@@ -24,26 +24,8 @@ module Lib.PipeUsingUniform where
 import           Test.Framework
 
 import qualified Pipes as Pipe
---import  Pipes
 import qualified Pipes.Prelude as Pipe
---import  Pipes.Prelude
 import Pipes ((>->), (~>))
-
---import qualified System.Directory as D
---import qualified System.Posix as Posix
---import   System.FilePath.Posix  ((</>))
---import Lib.NoPipe (processOneDirEntry, directoryContent, processOneFile
---                        , testDir, resTestDir6)
-
---import Uniform.Error
---import qualified Path         as Path
---import Path.IO  as PIO
---import System.IO as S
---import System.Directory as D
---import System.FilePath.Posix (dropTrailingPathSeparator)
---import System.Posix as Posix
-
---import "monads-tf" Control.Monad.Error as CME
 
 import Uniform.FileIOalgebra
 import Uniform.FileStrings
@@ -51,8 +33,6 @@ import Uniform.FileStatus
 import Uniform.Error
 import Uniform.Strings hiding ((</>))
 
---import Lib.NoPipeUsingUniform (getMD5, testDir )
---import Lib.NoPipe  (getMD5)
 
 import Data.Maybe
 import Data.List (sort)
@@ -72,40 +52,13 @@ startPipe2 target store = do
     closeFile2 storeHandle
     return  ()
 
---effect ::  (ErrorType ((Pipe.Proxy
---                                         Pipe.X
---                                         ()
---                                         ()
---                                         String
---                                         (CME.ErrorT
---                                            Text IO))) ~ Text) =>
---                Path Abs Dir -> Handle -> Pipe.Effect ErrIO ()  -- useful to fix types
-effect target storeHandle = Pipe.for (initialProducer target)  $ \dir ->
---                initialProducer
+effect target storeHandle = Pipe.for (Pipe.yield target)  $ \dir ->
                 recurseDir dir
                 >-> Pipe.toHandle storeHandle
---                >-> finalConsumer
---                >-> Pipe.toListM
---                    $ target
 
---initialProducer :: Path Abs Dir -> Pipe.Producer (Path Abs Dir) ErrIO ()
---initialProducer :: Path Abs Dir -> Pipe.Proxy (Pipe.X) (Pipe.X) ()  (Path Abs Dir) IO ()
-initialProducer fp = do
-    Pipe.yield fp
-
---finalConsumer ::  Pipe.Consumer String ErrIO ()
---finalConsumer = do
---    st <- Pipe.await
---    Pipe.lift $ callIO $  putStrLn st
---    finalConsumer
-
-
---processOneFile ::  -- (ErrorType (Pipe.Proxy x x' y y' ErrIO  ) ~ Text)
---          Path Abs File -> Pipe.Pipe (Pipe.X) String ErrIO ()
---processOneFile :: Path Abs File -> Pipe.Proxy (Pipe.X) (Pipe.X) ()  String IO ()
--- ^ process one file - print filename as a stub
 processOneFile ::Path b File
                       -> Pipe.Proxy Pipe.X () () String (ErrorT Text IO) ()
+-- ^ process one file - print filename as a stub
 processOneFile fn = do
 --    putIOwords ["processOneFile test ", showT fn]
     fx <- Pipe.lift $  doesFileExist' fn
@@ -117,25 +70,15 @@ processOneFile fn = do
             perm <-Pipe.lift $ getPermissions' fn
             res <- if readable perm
                 then  do
-        --                putIOwords ["processOneFile test ", showT fn, "readable", showT isReadable]
                         md <- Pipe.lift $   getMD5 (toFilePath fn)
                         let res = unwords' ["\nF:", showT fn, showT md]
-        --                putIOwords ["processOneFile done ", showT fn, "readable", showT isReadable]
                         return (Just res)
-        --            `catch` \(e :: SomeException)  -> do
-        --                putIOwords ["processOneFile error ", showT fn, "readable", showT perm, "\n", showT e]
-        --                throwErrorT ["processOneFile - problem with getMD5", showT e]
-                        -- could be simply
---                        return . Just . unwords' $ ["\nF:", show' fn, ""]
                 else do
                     Pipe.lift $ putIOwords ["processOneFile not readable ", showT fn]
                     return Nothing
             Pipe.yield (maybe "" t2s res)
             return () -- (show res)
 
---xisSymbolicLink t =  D.pathIsSymbolicLink   (dropTrailingPathSeparator $ toFilePath t)
-
---recurseDir :: Path Abs Dir  -> Pipe.Pipe (Pipe.X) String IO ()
 --recurseDir ::  (ErrorType  (Pipe.Proxy (Pipe.X) (Pipe.X) ()  String ErrIO) ~ Text) =>
 --    Path Abs Dir  -> Pipe.Proxy (Pipe.X) (Pipe.X) ()  String ErrIO ()
 -- must not have a defined type
@@ -145,29 +88,20 @@ recurseDir fp = do
 --    putIOwords ["recurseDir start", showT fp]
     perm <-Pipe.lift $ getPermissions' fp
     if not (readable perm && searchable perm)
-        then do
-            Pipe.lift $ putIOwords ["recurseDir not readable or not searchable", showT fp]
---            throwError ("something" :: Text)
-            return ()
+        then Pipe.lift $ putIOwords ["recurseDir not readable or not searchable", showT fp]
         else do
             symLink <- Pipe.lift $ checkSymbolicLink fp -- callIO $ xisSymbolicLink fp
             if symLink
-                then do
-                        Pipe.lift $ putIOwords ["recurseDir symlink", showT fp]
-                        return ()
+                then  Pipe.lift $ putIOwords ["recurseDir symlink", showT fp]
                 else do
                     let res1 = unwords' ["\nD: ", show'  fp]
-            --         res1 :: Text <- Pipe.lift $ processOneDirEntry fp  -- callIO just to get error
                     Pipe.yield res1
                     (dirs, files) <- Pipe.lift   $ listDir  fp
                     Prelude.mapM_ processOneFile (sort files)
                     Prelude.mapM_ recurseDir (sort dirs)
                     return ()
 
---        return ()
-----    `catch` \(e::SomeException) -> do
-----            putIOwords ["recurseDir - error catch: ", showT e]
-----            return ()
+-------------- tests
 
 resFile0 = fromJustNote "testdir1" $ parseRelFile "result20"
 resFileN = fromJustNote "testdir2" $ parseRelFile "result2N" :: Path Rel File
